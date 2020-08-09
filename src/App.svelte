9edc8669f15c4293e31620027bcd5e6ee7a5c5be
Ben <34322681+TLClassless@users.noreply.github.com>
2020-08-09 17:57:32 +0100

    starting styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,63 @@
-<script></script>
+<script>
 
+</script>
 
+<span>
+
+</span>
+
+<span class="visual">
+	<p> // </p>
+</span>
 
 <h1>
-	The Tarot
+	Placeholder
 </h1>
+
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat nunc quis tellus rhoncus ullamcorper.
+</p>
+
+<div class="nav">
+
+<button>
+	<p>Back</p>
+</button>
+
+<button>
+	<p>Next</p>
+</button>
+
+</div>
 
 
 <style> 
 	h1 {
+		margin: 10px;
+		display: flex;
 		font-family: 'PT Serif', serif;
 		text-align: center;
+		justify-content: center;
+	}
+
+	p {
+		margin: 10px;
+		display: flex;
+		font-family: 'PT Serif', serif;
+		text-align: center;
+		justify-content: center;
+	}
+
+	.visual {
+		justify-content: center;
+		display: flex;
+		height: 60%;
+		width: 100%;
+	}
+
+	.nav {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 	}
 </style>
