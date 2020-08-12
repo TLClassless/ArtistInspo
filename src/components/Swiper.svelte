7fa28c65c7adcaf1987632e0c3be807a9c1bda0a
Ben <34322681+TLClassless@users.noreply.github.com>
2020-08-12 18:57:34 +0100

    testing swiper functionality

--- src/components/Swiper.svelte
+++ src/components/Swiper.svelte
@@ -1,6 +1,10 @@
 <script>
       import { Swipe, SwipeItem } from "svelte-swipe"; // gzipped 3.37 KB
 
+let placeholder1 = "img/placeholder 1.png"
+let placeholder2 = "img/placeholder 2.png"
+let placeholder3 = "img/placeholder 3.png"
+
 let autoplay = false;
 let delay = 2000; //ms
 let showIndicators = true;
@@ -11,24 +15,20 @@ let defaultIndex = 0; //start from 0
 <div class="swipe-holder">
     <Swipe {showIndicators} {autoplay} {delay} {transitionDuration} {defaultIndex}>
       <SwipeItem>
-        <img src="./images/1.jpg" alt="">
+        <img src={placeholder1} alt="">
       </SwipeItem>
   
       <SwipeItem>
-        <img src="./images/2.jpg" alt="">
+        <img src={placeholder2} alt="">
       </SwipeItem>
   
       <SwipeItem>
-        <img src="./images/3.jpg" alt="">
+        <img src={placeholder3} alt="">
       </SwipeItem>
     </Swipe>
 </div>
 
 <style>
-  .swipe-holder{
-    height: 30vh;
-    width: 100%;
-  }
   img{
     max-width: 100%;
     height: auto;
