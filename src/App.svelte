0d9db8a88b5b6c8d2ed720a7efffe8bd5042d08a
Ben <classle55sc2@gmail.com>
2021-03-08 16:09:51 +0000

    added margin to the top of the slide, for a better desktop experience

--- src/App.svelte
+++ src/App.svelte
@@ -12,12 +12,19 @@
 		max-width: 450px;
 		max-height: 900px;
 		min-height: 600px;
+		overflow: hidden;
 	}
 
 	@media screen and (min-width: 800px) {
 		body {
 			box-shadow: 0 0 50px 15px grey;
 			display: flex;
+		}
+	}
+
+	@media screen and (min-height:1000px) {
+		body {
+			margin-top: 3%;
 		}
 	}
 </style>
